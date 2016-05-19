5694655d24ad9ac259e7a698e373740253130dac
BigBorg <770188954@qq.com>
2016-05-19 20:17:44 +0800

    Version 1.1
    New version

--- R/loadMiData.R
+++ R/loadMiData.R
@@ -2,7 +2,7 @@
 #' @description  Load in MiBand data from databases directory. Extract steps, slight sleep and deep sleep data as a data frame. Fill in missing data with average values of the same day in a week.
 #' @param path a character string specifing the "databases" directory which is copied from android system /data/data/com.xiaomi.hm.health. Example: If folder databases is in home directory, then path should be "~/databases".
 #' @param user_id a caracter string specifying your id. User id can be accessed in the user info page in MiBand android app. Or you can find it as a part of file name specifying the origin_db file. For example, you may find a file named as "origin_db_123456" in the databases directory, then parse character string "123456" as user_id.
-#' @return return a list with element "rawdata" as a data frame with missing value and "completedata" as a data frame with missing data substitued.
+#' @return return a list with element "cleandata" as a data frame with missing value and "data_week" as a data frame with missing data substitued.
 #' @export
 #' @import DBI RSQLite dplyr jsonlite
 #' @author Borg
@@ -36,20 +36,23 @@ loadMiData <- function(path,user_id){
         lightsleep<-unname( unlist( lapply(slp,"[[","lt") )  )   ##extract elements from list of list
         deepsleep<-unname( unlist( lapply(slp,"[[","dp") )  )
         totalstep<-unname( unlist( lapply(stp,"[[","ttl") ) )
-        cleanData <- data.frame(date=sourceData$DATE,sleep.light=lightsleep,sleep.deep=deepsleep,step=totalstep)
+        cleanData <- data.frame(date=sourceData$DATE,sleep.light=lightsleep,sleep.deep=deepsleep,step=totalstep,efficiency=deepsleep/(deepsleep+lightsleep) )
         cleanData<-dplyr::arrange(cleanData,date)
 
-        # Fill in Missing Data
+        # Fill by mean of week
         cleanData$weekday<-weekdays(cleanData$date)
+        cleanData$weekday <- as.factor(cleanData$weekday)
+        levels(cleanData$weekday) <- c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
         groupeddata<-group_by(cleanData,weekday)
         avgs<-summarize(groupeddata,
                         sleep.light=mean(sleep.light[sleep.light!=0]),
                         sleep.deep=mean(sleep.deep[sleep.deep!=0]),
-                        step=mean(step[step!=0]))
+                        step=mean(step[step!=0]),
+                        efficiency=mean(efficiency,na.rm = TRUE))
         replace <- function(rowunit,avgs){
                 for( col in 1:ncol(rowunit) ){
-                        if(rowunit[,col]==0){
-                                rowunit[,col]<-avgs[avgs[,1]==rowunit$weekday,col]
+                        if(is.nan(rowunit[,col]) | rowunit[,col]==0){
+                                rowunit[,col]<-avgs[avgs[,1]==as.character(rowunit$weekday),col]
                         }
                 }
                 rowunit
@@ -57,10 +60,15 @@ loadMiData <- function(path,user_id){
         completedata<-by(cleanData,1:nrow(cleanData),function(rowunit) replace(rowunit,avgs))
         completedata<-as.data.frame(do.call("rbind",completedata))
 
+        #TO-DO: fill missing data by mean of month
+        #TO-DO: fill missing data by mean of the same week
+        #TO-DO: fill missing data by mean of the same month
         # Use NA as missing data indicator
+
         for(i in seq(ncol(cleanData))){
                 cleanData[,i][cleanData[,i]==0] <- NA
         }
+
         Sys.setlocale("LC_TIME",baklocale)
-        return(list(rawdata=cleanData,completedata=completedata))
+        return(list(data_clean=cleanData,data_week=completedata,avg_week=avgs))
 }
---
+++ R/miPlot.R
@@ -0,0 +1,80 @@
+#' @title Mi Plot
+#' @description Generic plot function for Mi Data. Using rChart library to plot.
+#' @param MiData is a list returned by function loadMiData
+#' @param type character indicating type of plot. Options are "hist", "ts", "week", "box". "week" plots mean value of the same day in a week, such as mean value of Monday.
+#' @param  y variable to be plotted as y value. Options are "light", "deep", "sleep", "step". "Sleep" means to plot both light sleep and deep sleep as y values.
+#' @param  col color to be parsed to ggplot function.
+#' @param bins only used in hist
+#' @return ggplot object
+#' @export
+#' @author Borg
+
+miPlot <- function(MiData,type,y,col="blue",bins=30,by="week",fill="#575757"){
+        # plottype: hist_light, hist_deep, hist_step, hist_efficiency
+        #           ts_light, ts_deep, ts_light+deep, ts_step, ts_efficiency
+        #           week_light, week_deep, week_step, week_efficiency
+        #TO-DO      month_light, month_deep, month_step
+        #           box_light_deep
+        switch (by,
+                'week' = {data_plot<-MiData$data_week}
+        )
+        switch (type,
+                "hist" = {
+                        switch (y,
+                                'step' = {p <- ggplot(data = data_plot,aes(step))+geom_histogram(aes(y=..density..),col=col,bins=bins,fill=fill)+geom_density(stat = "density")+labs(title="Step Histogram")},
+                                'light' = {p <- ggplot(data=data_plot,aes(sleep.light/60)) + geom_histogram(aes(y=..density..),col=col,fill=fill,bins=bins) + geom_density(stat= "density") + labs(title="Light Sleep Histogram") },
+                                'sleep' ={
+                                        data_plot=melt(data_plot)
+                                        p<- ggplot(data_plot,aes(x=value)) + geom_histogram(data=filter(data_plot,variable=="sleep.light"),fill="red",alpha=0.4,bins=bins) + geom_histogram(data=filter(data_plot,variable=="sleep.deep"),fill="blue",alpha=0.4,bins=bins)
+                                },
+                                'deep' = {p <- ggplot(data=data_plot,aes(sleep.deep/60)) + geom_histogram(aes(y=..density..),col=col,bins=bins,fill=fill) + geom_density(stat= "density") + labs(title="Light Sleep Histogram") },
+                                'efficiency' = {p <- ggplot(data=data_plot,aes(sleep.efficiency)) + geom_histogram(aes(y=..density..),col=col,bins=bins,fill=fill) + geom_density(stat= "density") + labs(title="Sleep Efficiency Histogram") }
+                        )
+                },
+                "ts"={
+                        switch (y,
+                                'step' = {p <- ggplot(data = data_plot,aes(x=date,y=step))+ geom_line(col=col) +labs(title="Step Time Sequence")},
+                                'light' = {p <- ggplot(data=data_plot,aes(x=date,y=sleep.light/60)) + geom_line(col=col) +labs(title="Light Sleep Time Sequence")},
+                                'deep' = {p <- ggplot(data=data_plot,aes(x=date,y=sleep.deep/60)) + geom_line(col=col) +labs(title="Deep Sleep Time Sequence")},
+                                'efficiency' = {p <- ggplot(data=data_plot,aes(x=date,y=efficiency)) + geom_line(col=col) +labs(title="Efficiency Time Sequence")},
+                                'sleep' = {p <- ggplot(data=data_plot,aes(x=date,y=variable,col=type)) + geom_line(aes(y=sleep.light/60,col="sleep.light")) + geom_line(aes(y=sleep.deep/60,col="sleep.deep")) +labs(title="Sleep Time Sequence")}
+                        )
+                },
+                "week"={
+                        data_plot<-MiData$avg_week
+                        switch (y,
+                                'step' = {p <- ggplot(data = data_plot,aes(x=weekday,y=step))+ geom_bar(col=col,stat="identity",fill=fill) +labs(title="Step Week Mean")},
+                                'light' = {p <- ggplot(data=data_plot,aes(x=weekday,y=sleep.light/60)) + geom_bar(col=col,fill=fill) +labs(title="Light Sleep Week Mean")},
+                                'deep' = {p <- ggplot(data=data_plot,aes(x=weekday,y=sleep.deep/60)) + geom_bar(col=col,fill=fill) +labs(title="Deep Sleep Week Mean")},
+                                'efficiency' = {p <- ggplot(data=data_plot,aes(x=weekday,y=efficiency)) + geom_bar(col=col,fill=fill) +labs(title="Efficiency Week Mean")},
+                                'sleep' = {p <- ggplot(data=data_plot,aes(x=weekday,y=variable,col=type)) + geom_bar(aes(y=sleep.light/60,col="sleep.light"),stat = "identity") + geom_bar(aes(y=sleep.deep/60,col="sleep.deep"),stat="identity") +labs(title="Sleep Week Mean")}
+                        )
+                },
+                'box'={
+                        switch (y,
+                                'sleep' = {
+                                        data_plot <- dplyr::filter(melt(data_plot),variable=="sleep.deep"|variable=="sleep.light")
+                                        p <- ggplot(data_plot,aes(x=variable,y=value/60)) + geom_boxplot(aes(fill=variable))+labs(title="Sleep Box Plot")+labs(y="duration(hour)")
+                                }
+                                ,
+                                'light' = {
+                                        data_plot <- dplyr::filter(melt(data_plot),variable=="sleep.light")
+                                        p <- ggplot(data_plot,aes(x=variable,y=value/60)) + geom_boxplot(aes(fill=fill))+labs(title="Light Sleep Box Plot")+labs(y="light sleep(hour)")
+                                },
+                                'deep' = {
+                                        data_plot <- dplyr::filter(melt(data_plot),variable=="sleep.deep")
+                                        p <- ggplot(data_plot,aes(x=variable,y=value/60)) + geom_boxplot(aes(fill=fill))+labs(title="Deep Sleep Box Plot")+labs(y="deep sleep(hour)")
+                                },
+                                'step' = {
+                                        data_plot <- dplyr::filter(melt(data_plot),variable=="step")
+                                        p <- ggplot(data_plot,aes(x=variable,y=value/60)) + geom_boxplot(aes(fill=fill))+labs(title="Step Box Plot")+labs(y="step count")
+                                },
+                                'efficiency' = {
+                                        data_plot <- dplyr::filter(melt(data_plot),variable=="efficiency")
+                                        p <- ggplot(data_plot,aes(x=variable,y=value/60)) + geom_boxplot(aes(fill=fill))+labs(title="Efficiency Box Plot")+labs(y="Efficiency")
+                                }
+                        )
+                }
+        )
+        return(p)
+}
